e4775e830c9e1e37153e6ce3fd690f5232f5576c
nguyentrinh2611 <nguyentrinha3qt@gmail.com>
2026-03-31 10:38:04 +0700

    refactor: replace imperative bootstrap logic with BlocListener for navigation in SplashPage

--- packages/splash/lib/presentation/pages/splash_page.dart
+++ packages/splash/lib/presentation/pages/splash_page.dart
@@ -16,38 +16,18 @@ class SplashPage extends StatefulWidget {
 
 class _SplashPageState extends State<SplashPage> {
   @override
-  void initState() {
-    super.initState();
-    _bootstrap();
-  }
-
-  Future<void> _bootstrap() async {
-    UserBloc userBloc = context.read<UserBloc>();
-    Future<bool> isAuth() async {
-      if (userBloc.state.userType == UserType.success) return true;
-      if (userBloc.state.userType == UserType.error) return false;
-
-      // Nếu đang loading thì mới chờ stream
-      final state = await userBloc.stream.firstWhere(
-        (s) => s.userType == UserType.success || s.userType == UserType.error,
-      );
-
-      return state.userType == UserType.success;
-    }
-
-    final authenticated = await isAuth();
-
-    if (authenticated && mounted) {
-      context.router.replace(const MainRoute());
-    } else if (mounted) {
-      context.router.replace(const LoginRoute());
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
     final theme = context.theme;
     final appColor = theme.appColors;
-    return Container(color: appColor.green[700]);
+    return BlocListener<UserBloc, UserState>(
+      listener: (context, state) {
+        if (state.userType == UserType.success) {
+          context.router.replace(const MainRoute());
+        } else if (state.userType == UserType.error) {
+          context.router.replace(const LoginRoute());
+        }
+      },
+      child: Container(color: appColor.green[700]),
+    );
   }
 }
